1c8bafb0dc438d7e84f7ad1b1cf18e2859980e9b
Andrew Bentzen <bentzen.an@gmail.com>
2022-03-22 16:22:25 -0400

    adds stub pages for login and pref pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'home#index'
-
-
+  get '/home', to: 'home#index'
+  get '/login', to: 'login#index'
+  get '/preferences', to: 'preferences#index'
 end
